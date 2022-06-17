9f680ac47705f84f45e97379d7fb7da1831f5f54
Moise1 <rwibutso.mo@gmail.com>
2022-06-17 10:10:08 +0200

    initial commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args){
+       final byte MONTHS_IN_YEAR = 12;
+       final byte PERCENT = 100;
+
+       Scanner scanner = new Scanner(System.in);
+
+       System.out.print("Principal: ");
+       int principal = scanner.nextInt();
+
+       System.out.print("Annual interest rate: ");
+       float annualInterest = scanner.nextFloat();
+       float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+       System.out.print("Period(years): ");
+       byte years = scanner.nextByte();
+       int numberofPayments = years * MONTHS_IN_YEAR;
+
+       double mortgage = principal *
+                        (monthlyInterest * Math.pow(1 + monthlyInterest, numberofPayments))
+                        / (Math.pow(1 + monthlyInterest, numberofPayments));
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " +  mortgageFormatted);
+    }
+}
+
+
+  
+// Stopped at 1:36:21
